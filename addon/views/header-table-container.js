0a23dfe585735bf22220aefefc2f3cfb00951264
Albert Treat <atreat@zenefits.com>
2016-04-28 16:38:40 -0700

    more updates

--- addon/views/header-table-container.js
+++ addon/views/header-table-container.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 import TableContainer from 'ember-table/views/table-container';
+import ShowHorizontalScrollMixin from 'ember-table/mixins/show-horizontal-scroll';
 import RegisterTableComponentMixin from 'ember-table/mixins/register-table-component';
 
-export default TableContainer.extend(RegisterTableComponentMixin, {
+export default TableContainer.extend(ShowHorizontalScrollMixin, RegisterTableComponentMixin, {
   templateName: 'header-table-container',
   classNames: ['et-table-container',
       'et-fixed-table-container',
