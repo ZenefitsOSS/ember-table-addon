356dc113acd9688009cb264c9a7731df7cb52fb6
Reid Roman <rroman@zenefits.com>
2016-10-03 20:57:02 -0700

    antiscrollbars

--- addon/views/table-block.js
+++ addon/views/table-block.js
@@ -4,17 +4,16 @@ import RegisterTableComponentMixin from 'ember-table/mixins/register-table-compo
 
 // TODO: This should be a mixin
 export default Ember.CollectionView.extend(
-StyleBindingsMixin, RegisterTableComponentMixin, {
-  classNames: ['et-table-block'],
-  styleBindings: ['width', 'height'],
-  itemViewClass: Ember.computed.alias('tableComponent.tableRowViewClass'),
-  height: Ember.computed.oneWay('tableComponent._headerHeight'),
-  columns: null,
-  content: null,
-  scrollLeft: null,
+  StyleBindingsMixin, RegisterTableComponentMixin, {
+    classNames: ['et-table-block'],
+    styleBindings: ['width', 'height'],
+    itemViewClass: Ember.computed.alias('tableComponent.tableRowViewClass'),
+    height: Ember.computed.oneWay('tableComponent._headerHeight'),
+    columns: null,
+    content: null,
+    scrollLeft: null,
 
-  onScrollLeftDidChange: Ember.observer(function() {
-    return this.$().scrollLeft(this.get('scrollLeft'));
-  }, 'scrollLeft')
-
-});
+    onScrollLeftDidChange: Ember.observer(function() {
+      return this.$().scrollLeft(this.get('scrollLeft'));
+    }, 'scrollLeft')
+  });
